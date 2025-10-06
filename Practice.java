6e92756620750f8eb05c71d9d43d0d66dafc3390
AtulSingh205 <atulsingh07062004@gmail.com>
2025-10-06 19:27:33 +0530

    update

--- Practice.java
+++ Practice.java
@@ -4,15 +4,15 @@
 //     }
 // }
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class Practice {
-    public static void main(String[] args) {
-        Scanner inp = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int n = inp.nextInt();
-        System.out.print("Enter a number: ");
-        int m = inp.nextInt();
-        System.out.println("And the of number is: "+(n+m));
-    }
-}
+// public class Practice {
+//     public static void main(String[] args) {
+//         Scanner inp = new Scanner(System.in);
+//         System.out.print("Enter a number: ");
+//         int n = inp.nextInt();
+//         System.out.print("Enter a number: ");
+//         int m = inp.nextInt();
+//         System.out.println("And the of number is: "+(n+m));
+//     }
+// }
