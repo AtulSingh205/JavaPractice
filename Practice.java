fa645c5ecfc05409177f18f9c582c46e01056d1b
AtulSingh205 <atulsingh07062004@gmail.com>
2025-10-07 11:29:04 +0530

    update

--- Practice.java
+++ Practice.java
@@ -16,3 +16,31 @@
 //         System.out.println("And the of number is: "+(n+m));
 //     }
 // }
+
+import java.util.Scanner;
+
+class triangel{
+    public void display(int x ,int y , int z ){
+           if(x+y>z||y+z>x|| z+x>y){
+            System.out.println("Valid Traingel");
+           }
+           else{
+            System.out.println("Invalid Triangel");
+           }
+    }
+}
+
+public class Practice {
+public static void main(String[] args) {
+    Scanner inp = new Scanner(System.in);
+    System.out.print("Enter a number ");
+    int a = inp.nextInt();
+    System.out.print("Enter b number: ");
+    int b = inp.nextInt();
+    System.out.print("Enter c number: ");
+    int c = inp.nextInt();
+    triangel obj = new triangel();
+    obj.display(a, b, c);
+}
+    
+} 
