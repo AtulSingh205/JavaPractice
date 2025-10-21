6b2f8b61098fa4734a418e743a5e8488f9d701de
AtulSingh205 <atulsingh07062004@gmail.com>
2025-10-21 19:11:12 +0530

    triangel

--- Practice.java
+++ Practice.java
@@ -494,15 +494,17 @@
 
 // -----------------haafreversetriangel--------------
 
-public class Practice {
+// public class Practice {
 
-   public static void main(String[] args) {
-      int n = 5;
-      for(int i =1;i<=n;i++){
-         for(int j = 1;j<=n;j++){
-              System.out.print("*");
-         }
-         System.out.println(" ");
-      }
-   }
-}
+//    public static void main(String[] args) {
+//       int n = 5;
+//       for(int i =1;i<=n;i++){
+//          for(int j = 1;j<=n;j++){
+//               System.out.print("*");
+//          }
+//          System.out.println(" ");
+//       }
+//    }
+// }
+
+// ---------------
