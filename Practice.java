bdd62920eb2fa9522b262d751e8e82c80e00e2e5
AtulSingh205 <atulsingh07062004@gmail.com>
2025-10-23 10:50:29 +0530

    plus

--- Practice.java
+++ Practice.java
@@ -527,3 +527,24 @@
 //    else System.out.println("Please ente a odd number ");
 //    }
 // }
+
+// --------------------plus sign----------------
+public class Practice {
+
+   public static void main(String[] args) {
+      int n =5;
+      for(int i = 1;i<=n;i++){
+         if(i!=3){
+            for(int k =1;k<=2;k++){
+               System.out.print(" ");
+            }
+         }
+         for(int j = 1;j<=n;j++){
+            if(j==3||i==3){
+               System.out.print("*");
+            }
+         }
+         System.out.println();
+      }
+   }
+}
