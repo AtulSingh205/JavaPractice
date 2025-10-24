f1417cba69a469cf568d0069bc0635825d7b6b78
AtulSingh205 <atulsingh07062004@gmail.com>
2025-10-24 06:32:36 +0530

    update

--- Practice.java
+++ Practice.java
@@ -570,14 +570,34 @@
 // -------------------number patern-----------
 
 
+// public class Practice {
+
+//     public static void main(String[] args) {
+//         int cnt = 1;
+//         for(int i =1;i<=4;i++){
+//             for (int j = 1;j<=i;j++ ){
+//                  System.out.print(cnt);
+//                  cnt++;
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// ----------binary triangel---------------
+
 public class Practice {
 
     public static void main(String[] args) {
-        int cnt = 1;
-        for(int i =1;i<=4;i++){
-            for (int j = 1;j<=i;j++ ){
-                 System.out.print(cnt);
-                 cnt++;
+        int n = 4;
+        for (int i = 1;i<=n;i++){
+            for(int j = 1;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print("1");
+                }
+                else{
+                    System.out.print("0");
+                }
             }
             System.out.println();
         }
