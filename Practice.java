8256aed70ee96b71a2e480ee5e7836077907629a
AtulSingh205 <atulsingh07062004@gmail.com>
2025-10-24 06:21:25 +0530

    trainge;

--- Practice.java
+++ Practice.java
@@ -529,22 +529,57 @@
 // }
 
 // --------------------plus sign----------------
+// public class Practice {
+
+//    public static void main(String[] args) {
+//       int n =5;
+//       for(int i = 1;i<=n;i++){
+//          if(i!=3){
+//             for(int k =1;k<=2;k++){
+//                System.out.print(" ");
+//             }
+//          }
+//          for(int j = 1;j<=n;j++){
+//             if(j==3||i==3){
+//                System.out.print("*");
+//             }
+//          }
+//          System.out.println();
+//       }
+//    }
+// }
+
+// ----------------------cros pattern--------------
+
+// public class Practice {
+
+//    public static void main(String[] args) {
+//       int n = 5;
+//       for(int i = 1;i<=n;i++){
+//          if(i)
+//          for(int j = 1;j<=n;j++){
+//             if(i==j){
+//             System.out.print("*");
+//             }
+//          }
+//          System.out.println();
+//       }
+//    }
+// }
+
+// -------------------number patern-----------
+
+
 public class Practice {
 
-   public static void main(String[] args) {
-      int n =5;
-      for(int i = 1;i<=n;i++){
-         if(i!=3){
-            for(int k =1;k<=2;k++){
-               System.out.print(" ");
+    public static void main(String[] args) {
+        int cnt = 1;
+        for(int i =1;i<=4;i++){
+            for (int j = 1;j<=i;j++ ){
+                 System.out.print(cnt);
+                 cnt++;
             }
-         }
-         for(int j = 1;j<=n;j++){
-            if(j==3||i==3){
-               System.out.print("*");
-            }
-         }
-         System.out.println();
-      }
-   }
+            System.out.println();
+        }
+    }
 }
