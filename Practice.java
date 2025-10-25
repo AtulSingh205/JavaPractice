b443f83007ccc29cdd25cee732568a1290098318
AtulSingh205 <atulsingh07062004@gmail.com>
2025-10-25 05:57:02 +0530

    pyramid

--- Practice.java
+++ Practice.java
@@ -608,12 +608,12 @@
 public class Practice {
 
     public static void main(String[] args) {
-        int n = 3;
+        int n = 5;
         for(int i = 1;i<=n;i++){
             for(int j = 1;j<=n-i;j++){
                 System.out.print(" ");
             }
-            for(int k = 1;k<=i;k++){
+            for(int k = 1;k<=i*2-1;k++){
                 System.out.print("*");
             }
             System.out.println();
