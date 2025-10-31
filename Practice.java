ded03a5f8681cc92f74f9d8f8155a53442b2c731
AtulSingh205 <atulsingh07062004@gmail.com>
2025-10-31 18:52:14 +0530

    update

--- Practice.java
+++ Practice.java
@@ -997,13 +997,16 @@
 public class Practice {
 
   public static void main(String[] args) {
-    int[] arr={1,5,8,-3};
-    int max = 0;
-    int smax = 0;
+    int[] arr={1,5,8,-3,8};
+    int max = arr[0];
+    int smax = arr[0];
     for(int i =0;i<arr.length;i++){
       if(max<arr[i]){
         smax=max;
         max=arr[i];
+      }
+      else if (arr[i] > smax && arr[i] != max) {
+        smax = arr[i];
       }
     }
     System.out.println("Second max: "+smax);
