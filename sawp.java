b9033a325595b26f20a7902951caaa203c65b515
AtulSingh205 <atulsingh07062004@gmail.com>
2025-12-05 05:33:45 +0530

    update

--- sawp.java
+++ sawp.java
@@ -1226,25 +1226,46 @@
 
 // ----------------------segrigate2nd approch----------------
 
-public class Segrigate {
+// public class Segrigate {
 
-    public static void main(String[] args) {
-        int arr[] = {1,0,1,0,1,0};
-        int i =0;
-        int j =arr.length-1;
-        while (i<j) {
-            if(arr[i]==0) i++;
-            else if(arr[j]==1) j--;
-            else if(i>j) break;
-            else if(arr[i]==1 && arr[j]==0){
-                arr[i]=0;
-                arr[j]=1;
-                i++;
-                j--;
+//     public static void main(String[] args) {
+//         int arr[] = {1,0,1,0,1,0};
+//         int i =0;
+//         int j =arr.length-1;
+//         while (i<j) {
+//             if(arr[i]==0) i++;
+//             else if(arr[j]==1) j--;
+//             else if(i>j) break;
+//             else if(arr[i]==1 && arr[j]==0){
+//                 arr[i]=0;
+//                 arr[j]=1;
+//                 i++;
+//                 j--;
+//             }
+//         }
+//         for(int k= 0;k<arr.length;k++){
+//             System.out.print(arr[k]+" ");
+//         }
+//     }
+// }
+
+// ----------------------swap array-------------
+public class sawp {
+
+     public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        int i = 0;
+        int j = 1;
+        while(j<arr.length){
+                int temp = arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                i+=2;
+                j+=2;
             }
-        }
-        for(int k= 0;k<arr.length;k++){
+        for(int k=0;k<arr.length;k++){
             System.out.print(arr[k]+" ");
         }
-    }
+     }
 }
+
